6da2a933c006fec10365141af93ff2ccf7be7fd7
Mikael-W <mikaelwaw@gmail.com>
2022-10-20 18:48:13 +0200

    menu et dashboard

---
+++ src/components/AllEvents.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  props:{
+    allevents: false
+  }
+
+}
+</script>
+
+<template>
+  <div class="allevents_container">
+    <div class="event_container">
+      <div class="event_title">Mes évènements</div>
+      <div class="events"></div>
+    </div>
+    <div class="stats_container">
+      <div class="stats-title">Mes statistiques</div>
+      <div class="stats"></div>
+    </div>
+  </div>
+</template>
+
+
+
+<style>
+.allevents_container{
+  margin-left: 1.5vw;
+  width: 75vw;
+  height: 85vh;
+  background-color:black;
+  border-radius: 1rem;
+  display: flex;
+  
+}
+.event_container, .stats_container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.event_title, .stats-title{
+  margin-top: 1rem;
+  font-size:2rem;
+  font-weight: bold;
+  color: white;
+}
+.events, .stats{
+  margin: 1rem 0 0 2rem ;
+  width: 35vw;
+  height: 75vh;
+  background-color: #00C0FF;
+  border-radius: 1rem;
+}
+.eventStats{
+  margin-right: 2rem;
+}
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,18 +1,136 @@
-<script>
-import Menu from "../components/menu.vue"
+<script Setup>
+
+import AllEvents from '../components/AllEvents.vue'
+
+
 
 export default {
   name:"dashboard",
-  components:{Menu}
+  components:{
+    Menu, 
+    AllEvents},
+    data(){
+    return {
+    activeId: 0,
+      linksMenu:[
+        {id:1, title: 'Accueil'},
+        {id:2, title: 'Mes évènements payants'},
+        {id:3, title: 'Mes évènements gratuit'},
+        {id:4, title: 'Mon compte'},
+      ]}},
+      methods:{
+  selectLink(id) {              
+        this.activeId = id;
+        consol.log('clicked')
+        }
+    }
+
+
 }
 </script>
 
 <template>
-  <Menu />
+  <div class="dashboard_container">
+    <div class="nav_container">
+    <div class="logo_container">
+      <img src="../assets/logo_place2be.png" alt="">
+    </div>
+   
+    <div class="new-event_btn">
+      <img class="plus-icon" src="../assets/plus.png" alt="">
+      Nouvel évènement
+    </div> 
+    <nav>
+      <ul>
+        <div v-for="(link, index) in linksMenu" :key="index">
+        <li v-bind:class="{active:(link.id === activeId)}" @click="selectLink(link.id)">{{ link.title }}</li>
+        </div>
+        
+        <li>Déconnexion</li>
+      </ul>
+    </nav>
+  </div>
+   <AllEvents   />
+  </div>
+ 
+
+ 
 </template>
 
 
 
 <style>
+.dashboard_container{
+  display: flex;
+  align-items: center;
+}
+.nav_container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: black;
+    height: 100vh;
+    width: 20vw;
+  }
+
+  .logo_container{
+    background-color: #00C0FF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2rem;
+    border-radius: 50%;
+    width: 10vw;
+    height: 10vw;
+    
+  }
+
+  img{ 
+    height: 5vw;
+    object-fit: contain;
+  }
+  .plus-icon{
+    height: 1.5rem;
+  }
+  .new-event_btn{
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline: 2rem;
+    margin-top: 2rem;
+    color:black;
+    width: 10vw;
+    background-color: #00C0FF;
+    height: 5vh;
+  font-family: 'roboto', sans-serif;
+  border-radius: 5rem;
+  border: none;
+  }
+  nav{ 
+    margin-top: 2rem;
+    width: 100%;
+    display: flex;
+    justify-content:center
+  }
+  ul{ 
+    width: 100%;
+  }
+  ul li {
+    display: flex;
+    align-items: center;
+    padding: 1rem 0  1rem 5rem;
+    color:white;
+    width: 100%;
+    height: 3rem;
+  }
+
+  nav ul li:hover {
+    background-color: #00C0FF;
+  }
+  .active{
+    background-color: #00C0FF;
+  }
+  
+
 
 </style>
